0f9de7f184e7d6af859e3f4d7f75904262165baa
Paulo Brandao <paulobrandaoofficial@gmail.com>
2021-08-04 18:57:24 -0300

    Pacote testado

---
+++ TesteByCrypt/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BCrypt.Net;
+
+namespace TesteByCrypt.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            string passwordHash = BCrypt.Net.BCrypt.HashPassword("my password");
+
+            return Json(passwordHash);
+        }
+
+        [HttpGet("/hash")]
+        public IActionResult Get(string hash)
+        {
+            return Json(BCrypt.Net.BCrypt.Verify("my password", hash));
+        }
+    }
+}
